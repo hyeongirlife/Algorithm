cbcd002035ae4b7e247a068bc0433461c14f417d
hyeongirlife <leehyeongul@gmail.com>
2022-06-27 22:01:06 +0900

    핸드폰번호가리기.js repeat 메소드 이용한 풀이

--- 프로그래머스 LV1/핸드폰 번호 가리기.js
+++ 프로그래머스 LV1/핸드폰 번호 가리기.js
@@ -8,3 +8,14 @@ function solution(phone_number) {
 
   return first + second
 }
+
+// repeat 함수 이용한 결과
+function hide_numbers(s) {
+  var result = "*".repeat(s.length - 4) + s.slice(-4);
+  //함수를 완성해주세요
+
+  return result;
+}
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log("결과 : " + hide_numbers('01033334444'));
