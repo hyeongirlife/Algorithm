2d336b30a3b1e48ea6b4243b14acb60e84a44f7f
이현걸 <boo1996@naver.com>
2022-10-06 20:37:28 +0900

    Feat: [JS] 제일 작은 수 제거하기 삼항연산자 사용

--- 프로그래머스 LV1/제일 작은 수 제거하기.js
+++ 프로그래머스 LV1/제일 작은 수 제거하기.js
@@ -2,8 +2,5 @@ function solution(arr) {
   var answer = [];
   arr.sort((a, b) => b - a);
   answer.push(...arr.slice(0, arr.length - 1));
-  if (answer.length === 0) {
-    answer = [-1];
-  }
-  return answer;
+  return arr.length === 1 ? [-1] : answer;
 }
