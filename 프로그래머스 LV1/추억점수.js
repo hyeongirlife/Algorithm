d361ea0e757daa8765f1a618dbe0f8f50e9f7093
hyeongirlife <boo1996@naver.com>
2023-04-04 23:54:49 +0900

    Feat: [JS] 추억점수 리팩토링

--- 프로그래머스 LV1/추억점수.js
+++ 프로그래머스 LV1/추억점수.js
@@ -22,3 +22,20 @@ function solution(name, yearning, photo) {
 
   return result;
 }
+
+function solution(name, yearning, photo) {
+  const result = [];
+
+  const obj = {};
+  name.forEach((el, index) => {
+    obj[el] = yearning[index];
+  });
+  photo.forEach((arr) => {
+    let sum = 0;
+    for (let el of arr) {
+      sum += el in obj ? obj[el] : 0;
+    }
+    result.push(sum);
+  });
+  return result;
+}
